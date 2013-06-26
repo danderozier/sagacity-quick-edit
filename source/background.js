1866afe4eaa716b54f32dd353f8a8c734fe33cf1
Dan Derozier <derozier@gmail.com>
2013-06-26 10:11:12 -0500

    Adds reverse article support

--- source/background.js
+++ source/background.js
@@ -8,17 +8,23 @@ chrome.browserAction.onClicked.addListener( function(tab) {
 	var matchSlideshow = /http:\/\/(?:www\.)?(houstoniamag|seattlemet|portlandmonthlymag)\.com\/slideshows\/([a-z0-9-]+)/g;
 	var matchTags = /http:\/\/(?:www\.)?(houstoniamag|seattlemet|portlandmonthlymag)\.com\/tags\/([a-z0-9-]+)/g;
 	
+	var matchReverseArticle = /^http:\/\/(?:www\.)?(seattlemet|houstoniamag|portlandmonthlymag)\.com\/admin\/articles\/([a-z0-9-]+)\/edit/g;
+	
 	// Run the regexes and store matches
 	var resultEvent = matchEvent.exec(tab.url);	
 	var resultArticle = matchArticle.exec(tab.url);
 	var resultBusiness = matchBusiness.exec(tab.url);
 	var resultSlideshow = matchSlideshow.exec(tab.url);	
 	var resultTags = matchTags.exec(tab.url);
+
+	var resultReverseArticle = matchReverseArticle.exec(tab.url);
 	
 	var loc = '';
 		
 	// If we have matches, take the visitor to the edit page for that match type.
-	if( resultEvent )
+	if( resultReverseArticle )
+	  loc = 'http://www.' + resultReverseArticle[1] + '.com/articles/' + resultReverseArticle[2];
+	else if( resultEvent )
 		loc = 'http://www.' + resultEvent[1] + '.com/admin/calendar_events/' + resultEvent[2] + '/edit';		
 	else if( resultArticle )	
 		loc = 'http://www.' + resultArticle[1] + '.com/admin/articles/' + resultArticle[3] + '/edit';		
@@ -28,7 +34,6 @@ chrome.browserAction.onClicked.addListener( function(tab) {
 		loc = 'http://www.' + resultSlideshow[1] + '.com/admin/slideshows/' + resultSlideshow[2] + '/edit';				
 	else if( resultTags )
 		loc = 'http://www.' + resultTags[1] + '.com/admin/tags/' + resultTags[2] + '/edit';
-	
 	if( loc ) 
 		visitEditPage( tab, loc );
 		
